c7a68e7a905d5f6cd740e4a3222964db3a08a9a1
Maxim Siniavine <maxim@supercomputer.(none)>
2010-04-01 16:22:04 -0400

    Added concurrent test

--- test/test_my_mutex.c
+++ test/test_my_mutex.c
@@ -1,11 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
 #include "test.h"
 #include "my_mutex.h"
 
+struct my_mutex m;
+
+void* thread_func(void* arg)
+{
+	printf("Thread starting\n");
+	sleep(10);
+	printf("Releasing mutex\n");
+	up_mutex(&m);
+	return NULL;
+}
+
 int main()
 {
-	struct my_mutex m;
+	pthread_attr_t attr;
+	pthread_t thread;
+
 	init_my_mutex(&m);
+	
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	pthread_create(&thread, &attr, thread_func, NULL);
+
 	down_mutex(&m);
 	down_mutex(&m);
+	printf("Acquired mutex again\n");
+	pthread_join(thread, NULL);
 	return 0;
 }
