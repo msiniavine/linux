4c1917215efb75e548a82f547a35b64c4c7223a3
Maxim Siniavine <m.siniavine@gmail.com>
2011-01-04 16:35:41 -0500

    Added a utility to check if the restore needs to happen

---
+++ test/state_present.c
@@ -0,0 +1,10 @@
+#include "test.h"
+
+// program to check if state needs to be restored
+// exit code is 0 if no state is present
+// exit code is >0 if some state needs to be restored
+
+int main()
+{
+	return state_present();
+}
