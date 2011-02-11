5fc8aa9a7e0fb06e24a5c327cf1d2f843773c4fc
Maxim Siniavine <m.siniavine@gmail.com>
2011-02-11 15:55:27 -0500

    Updated scrips and configuration to remove useless messages

--- load_kernel.sh
+++ load_kernel.sh
@@ -3,7 +3,7 @@
 cd test
 {
     sleep 5
-    kexec -l /boot/vmlinuz-`uname -r` --append="root=/dev/sda1 1 irqpoll maxcpus=4 reset_devices load_state debug early_printk bootmem_debug"
+    kexec -l /boot/vmlinuz-`uname -r` --append="root=/dev/sda1 1 irqpoll maxcpus=4 reset_devices load_state"
     kexec -e
 } &
 
