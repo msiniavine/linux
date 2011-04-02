2256485fb8062153ec38bc89a9bb800ec5c427b3
Maxim Siniavine <m.siniavine@gmail.com>
2011-04-02 15:11:06 -0400

    Updated debug script file

--- load_kernel_gdb.sh
+++ load_kernel_gdb.sh
@@ -2,15 +2,15 @@
 
 cd test
 {
-    sleep 4
+    sleep 5
     kexec -l /boot/vmlinuz-`uname -r` --append="root=/dev/sda1 1 irqpoll maxcpus=4 reset_devices load_state kgdboc=ttyS0,115200 kgdbwait"
     kexec -e
 } &
 
-./test_terminal
+./test_sockerver &
 
-echo "All test started, rebooting kernel"
-sleep 1
+#echo "All test started, rebooting kernel"
+#sleep 1
 
 #kexec -l /boot/vmlinuz-`uname -r` --initrd=/boot/initrd.img-`uname -r` --append="root=/dev/sda1 1 irqpoll maxcpus=4 reset_devices load_state debug early_printk bootmem_debug"
 #kexec -l /boot/vmlinuz-`uname -r` --append="root=/dev/sda1 1 irqpoll maxcpus=4 reset_devices load_state kgdboc=ttyS0,115200 kgdbwait"
