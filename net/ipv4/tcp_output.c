b7277bd47fe48a8ac554ab331f84402c1af2d71e
Maxim Siniavine <m.siniavine@gmail.com>
2011-08-17 15:27:16 -0400

    Revert "Log retransmitted packets"
    This reverts commit 1dba5e47a4fa0296651e1041425289db9a2f26d8.

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2031,7 +2031,7 @@ int tcp_retransmit_skb(struct sock *sk, struct sk_buff *skb)
 	 */
 	TCP_SKB_CB(skb)->when = tcp_time_stamp;
 
-	sprint("Retransmit %u-%u\n", TCP_SKB_CB(skb)->seq, TCP_SKB_CB(SKB)->end_seq);
+	csprint("Retransmit\n");
 	err = tcp_transmit_skb(sk, skb, 1, GFP_ATOMIC);
 
 	if (err == 0) {
