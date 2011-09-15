d712f42892be32849d297694274cf36e6bebf4b7
Maxim Siniavine <m.siniavine@gmail.com>
2011-09-15 12:28:13 -0400

    Added a test where fork happens after restore

--- test/test_fork.c
+++ test/test_fork.c
@@ -7,6 +7,7 @@
 int main()
 {
 	int count = 1;
+	int forked_again = 0;
 	enable_save_state();
 	fork();
 	while(1)
@@ -14,6 +15,11 @@ int main()
 		sleep(1);
 		printf("%d\n", count);
 		count++;
+		if(was_state_restored() && !forked_again)
+		{
+			forked_again = 1;
+			fork();
+		}
 	}
 	return 0;
 }
