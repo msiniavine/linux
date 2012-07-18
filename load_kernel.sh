f73fe68f3e82deb980150e12803c63de4ba59cf7
Maxim Siniavine <m.siniavine@gmail.com>
2012-07-18 14:20:55 -0400

    Finished merging set state with ubuntu kernel

--- load_kernel.sh
+++ load_kernel.sh
@@ -8,7 +8,7 @@ done
 cd test
 {
     sleep 10
-    kexec -l /boot/vmlinuz-`uname -r` --append="root=/dev/sda1 1 irqpoll maxcpus=4 reset_devices load_state"
+    kexec -l /boot/vmlinuz-`uname -r` --append="root=/dev/sda1 maxcpus=4 reset_devices load_state"
     ./save_state
 } &
 
